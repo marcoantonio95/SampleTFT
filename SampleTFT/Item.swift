74f40d1da26306722d6a52c8380826f15fd31c63
Marco Antonio <Marco.antonio9858@gmail.com>
2025-01-09 23:20:26 -0300

    Initial Commit

---
+++ SampleTFT/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SampleTFT
+//
+//  Created by Marco Antonio on 09/01/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SampleTFTTests/SampleTFTTests.swift
@@ -0,0 +1,16 @@
+//
+//  SampleTFTTests.swift
+//  SampleTFTTests
+//
+//  Created by Marco Antonio on 09/01/25.
+//
+
+import Testing
+
+struct SampleTFTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
