50b02ccafb912063147ad8149d9d0335ebbefeb0
Marco Antonio <Marco.antonio9858@gmail.com>
2025-01-14 21:02:06 -0300

    feat: Create basic project struct

---
+++ SampleTFT/Presentation/View/ScreenOne.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenOne.swift
+//  SampleTFT
+//
+//  Created by Marco Antonio on 14/01/25.
+//
+
+import SwiftUI
+
+struct ScreenOne: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScreenOne()
+}
---
+++ SampleTFT/Presentation/View/ScreenTwo.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenTwo.swift
+//  SampleTFT
+//
+//  Created by Marco Antonio on 14/01/25.
+//
+
+import SwiftUI
+
+struct ScreenTwo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScreenTwo()
+}
