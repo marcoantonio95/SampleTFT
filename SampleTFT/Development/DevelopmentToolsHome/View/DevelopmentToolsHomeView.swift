fe913bcd966e874bcfe797d1a9861c2fb76f9aab
Marco Antonio <Marco.antonio9858@gmail.com>
2025-01-17 15:35:42 -0300

    feat: Create dev tools section

---
+++ SampleTFT/Development/DevelopmentToolsHome/View/DevelopmentToolsHomeView.swift
@@ -0,0 +1,41 @@
+//
+//  DevelopmentToolsHomeView.swift
+//  SampleTFT
+//
+//  Created by Marco Antonio on 17/01/25.
+//
+
+import SwiftUI
+
+struct DevelopmentToolsHomeView:
+    View {
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        NavigationStack {
+            VStack(alignment: .center) {
+                Spacer()
+                Text("Go to Request Mocker")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                Spacer()
+            }
+            .navigationTitle("Dev tools")
+            .navigationBarItems(leading:
+                 Image(systemName: "xmark")
+                .font(.title)
+                .scaledToFit()
+                .foregroundColor(.black)
+                .onTapGesture {
+                    dismiss()
+                }
+            )
+        }
+    }
+}
+
+#Preview {
+    DevelopmentToolsHomeView()
+}
